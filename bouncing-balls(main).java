2bbad97c4d7d63c58134ab36932741f2db1760a0
Muhammad Zakii Rofii <57617074+zakii-hub@users.noreply.github.com>
2021-12-29 18:11:02 +0800

    Modifikasi efek percikan dan gaya gesek
    Setiap bola berbenturan dengan bola lainnya maka akan mengeluarkan efek percikan serta berpantul sesuai arah, sudut dan kecepetan bola masing-masing. Kemudian memodifikasi gaya gesek antar bola sehingga bola dapat berpantul secara terus-menerus.

--- bouncing-balls(main).java
+++ bouncing-balls(main).java
@@ -9,7 +9,7 @@
 int numBalls = 12;
 float spring = 0.05;
 float gravity;
-float friction = -0.9;
+float friction = -1;
 Ball[] balls = new Ball[numBalls];
 
 void setup() {
@@ -19,8 +19,88 @@ void setup() {
     fill(random(255), random(255), random(255));
   }
   noStroke();
-  
+  ps = new ParticleSystem(new PVector(width/2, 50));
 }
+
+
+  // A simple Particle class
+
+class Particle {
+  PVector position;
+  PVector velocity;
+  PVector acceleration;
+  float lifespan;
+  
+
+  Particle(PVector l) {
+    acceleration = new PVector(0, 0.05);
+    velocity = new PVector(random(-1, 1), random(-2, 0));
+    position = l.copy();
+    lifespan = 255.0;
+  }
+
+  void run() {
+    update();
+    display1();
+  }
+
+  // Method to update position
+  void update() {
+    velocity.add(acceleration);
+    position.add(velocity);
+    lifespan -= 1.0;
+  }
+
+  // Method to display
+  void display1() {
+    stroke(255, lifespan);
+    fill(255, lifespan);
+    ellipse(position.x, position.y, 8, 8);
+  }
+ 
+ 
+ 
+  // Is the particle still useful?
+  boolean isDead() {
+    if (lifespan < 0.0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+  // A class to describe a group of Particles
+// An ArrayList is used to manage the list of Particles 
+ParticleSystem ps;
+class ParticleSystem {
+  ArrayList<Particle> particles;
+  PVector origin;
+
+  ParticleSystem(PVector position) {
+    origin = position.copy();
+    particles = new ArrayList<Particle>();
+  }
+
+  void addParticle() {
+    particles.add(new Particle(origin));
+  }
+
+  void run() {
+    for (int i = particles.size()-1; i >= 0; i--) {
+      Particle p = particles.get(i);
+      p.run();
+      if (p.isDead()) {
+        particles.remove(i);
+      }
+    }
+  }
+} 
+
+
+
+
+
 
 void draw() {
   background(0);
@@ -65,6 +145,9 @@ class Ball {
         vy -= ay;
         others[i].vx += ax;
         others[i].vy += ay;
+       
+       ps.addParticle();
+        ps.run();
       }
     }   
   }
